d9436a71b4f3c7d567aaca0ae74d039291de8653
pcbennett108 <pcbennett42@gmail.com>
2024-02-07 16:40:29 -0700

    Add Render backend deploy URL to cors config file.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:3000",
-            "http://127.0.0.1:3000"
+            "http://127.0.0.1:3000",
+            "https://m4-parks-backend.onrender.com"
 
             # "http://yourwebsite.production.app" frontend deploy URL
 
