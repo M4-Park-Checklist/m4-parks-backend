1820c149a2780f37cc120578cd1c285c1c61ddfa
Kam Kennedy <kameronk013@gmail.com>
2024-02-08 15:43:39 -0700

    add sad path for index

--- app/controllers/Api/V0/parks_controller.rb
+++ app/controllers/Api/V0/parks_controller.rb
@@ -1,9 +1,21 @@
 class Api::V0::ParksController < ApplicationController
   def index
-    render json: ParkSerializer.new(ParkFacade.get_parks)
+    data = ParkFacade.get_parks
+
+    if data.empty?
+      render json: {error: "No parks found"}, status: 404
+    else
+      render json: ParkSerializer.new(data)
+    end
   end
 
   def show
-    render json: ParkSerializer.new(ParkFacade.get_park(params[:id]))
+    if params[:id].nil?
+      render json: {error: "Invalid park id"}, status: 400
+    elsif params[:id].length != 4
+      render json: {error: "Invalid park id"}, status: 400      
+    else
+      render json: ParkSerializer.new(ParkFacade.get_park(params[:id]))
+    end
   end
 end
--- app/models/park.rb
+++ app/models/park.rb
@@ -4,4 +4,83 @@ class Park < ApplicationRecord
 
   has_many :user_parks
   has_many :users, through: :user_parks
+
+  attr_reader :id, 
+              :name,
+              :designation,
+              :description, 
+              :park_code,
+              :states, 
+              :media,  
+              :active_alerts, 
+              :things_to_do, 
+              :amenities, 
+              :hours_of_operation, 
+              :current_weather
+  
+  def initialize(data, single_park = false)
+    @id = data[:id]
+    @name = data[:fullName]
+    @designation = data[:designation]
+    @description = data[:description]
+    @park_code = data[:parkCode]
+    @states = data[:states]
+    @media = data[:images]
+    @single_park = single_park
+    
+    if @single_park == true
+      @active_alerts = self.get_active_alerts(data)
+      @things_to_do = self.get_things_to_do(data[:activities])
+      @amenities = self.get_amenities(data)
+      @hours_of_operation = self.get_hours_of_operation(data[:operatingHours])
+      @current_weather = self.get_weather(data[:addresses])
+    end
+  end
+
+  private
+
+  def get_things_to_do(activities)
+    activities_array = []
+    activities.each do |activity|
+      activities_array << activity[:name]
+    end
+    activities_array
+  end
+
+  def get_hours_of_operation(operating_hours)
+    hours_hash = {}
+    operating_hours.each do |hours|
+      hours_hash[hours[:name]] = hours[:standardHours]
+    end
+    hours_hash
+  end
+
+  def get_weather(addresses)
+    weather = WeatherService.get_weather(addresses)
+    weather[:data][:attributes][:current_weather]
+  end
+
+  def get_active_alerts(data)
+    alerts = ParkService.get_alerts(data[:parkCode])
+    
+    alert_summary = []
+    
+    alerts[:data].each do |alert|
+      alert_summary << alert[:title]
+    end
+    
+    alert_summary
+  end
+
+  def get_amenities(data)
+    amenities = ParkService.get_amenities(data[:parkCode])
+
+    amenity_summary = []
+    
+    amenities[:data].each do |amenity|
+      amenity_summary << amenity[:name]
+    end
+    
+    amenity_summary.uniq
+  end
 end
