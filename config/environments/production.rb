3f3b1173c5f94cce168ce33c933364a505f3fda8
Kam Kennedy <kameronk013@gmail.com>
2024-02-27 21:24:25 -0700

    mailer tested in development

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :user_name            => rails.application.credentials.gmail[:username],
-  :password             => rails.application.credentials.gmail[:password],
+  :user_name            => Rails.application.credentials.gmail[:username],
+  :password             => Rails.application.credentials.gmail[:password],
   :authentication       => "plain",
   :enable_starttls_auto => true
   }
