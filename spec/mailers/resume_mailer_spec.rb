7a9b32bdfe86edebf8e33b2fbb4aaad2ba518c5b
Kam Kennedy <kameronk013@gmail.com>
2024-02-27 21:16:51 -0700

    mailer tested

--- spec/mailers/resume_mailer_spec.rb
+++ spec/mailers/resume_mailer_spec.rb
@@ -1,18 +1,18 @@
 require "rails_helper"
+require "./spec/mailers/previews/resume_mailer_preview.rb"
 
 RSpec.describe ResumeMailer, type: :mailer do
   describe "resume_email" do
     let(:mail) { ResumeMailer.send_email("fake@domain.com") }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Resume email")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.subject).to eq("some fire resumes")
+      expect(mail.to).to eq(["fake@domain.com"])
+      expect(mail.from).to eq(["roosevelt@nationalparks.gov"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to include("If you're looking for a developer, please find links to our resumes below.")
     end
   end
-
 end
